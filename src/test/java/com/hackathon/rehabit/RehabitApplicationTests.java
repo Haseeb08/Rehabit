d7f03971dd7a966c843a3fd42aca4b3d6210aa0f
madhuri <madhuri.kotha@zemosolabs.com>
2020-04-17 17:38:37 +0530

    Project Setup

---
+++ src/test/java/com/hackathon/rehabit/RehabitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.rehabit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RehabitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
